aacbda190c43b8e143fe136beff226c7a2222249
Jose Rocher <jrocher@ieseljust.com>
2021-11-24 00:45:45 +0100

    Subimos el ejercicio Examen1eva

---
+++ examen1ev.java
@@ -0,0 +1,17 @@
+public class examen1ev {
+
+        /**
+     * @param args the command line arguments
+     */
+  
+    public static void main(String[] args){
+        
+        System.out.println("Hola a tots");
+        
+        for(int i=0;i<100;i++){
+            System.out.print(i+" ");
+        }
+    }
+    
+    
+}
